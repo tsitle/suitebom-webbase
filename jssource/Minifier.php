0ae03475f5914d808d0e566c23a2a0ca85606a3b
Thomas Sandkühler <technisandk@gmail.com>
2020-02-09 09:58:37 +0100

    Fix deprecated continue in switch in Minifier::breakdownScript()

--- jssource/Minifier.php
+++ jssource/Minifier.php
@@ -186,6 +186,8 @@ class Minifier
                 continue;
             }
 
+            $doCont = false;
+
             switch ($this->a) {
                 // new lines
                 case "\n":
@@ -237,13 +239,17 @@ class Minifier
                             // check for some regex that breaks stuff
                             if ($this->a == '/' && ($this->b == '\'' || $this->b == '"')) {
                                 $this->saveRegex();
-                                continue;
+                                $doCont = true;
+                            } else {
+                                echo $this->a;
+                                $this->saveString();
                             }
-
-                            echo $this->a;
-                            $this->saveString();
                             break;
                     }
+            }
+
+            if ($doCont) {
+                continue;
             }
 
             // do reg check of doom
